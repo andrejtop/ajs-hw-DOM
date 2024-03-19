779129254a5307b6475690cca2acd730d704569d
AndreyToptaev <Bonafide111@yandex.ru>
2024-03-19 10:38:17 +0300

    fix generate

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
 import Character from "./init";
+function generateGrid(container) {
+  for (let i = 0; i < 4; i++) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
+    for (let j = 0; j < 4; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("grid-cell");
+      row.appendChild(cell);
+    }
+    container.appendChild(row);
+  }
+}
+
+const container = document.querySelector(".grid-container");
+generateGrid(container);
 
 const Game = new Character(document.querySelector(".grid-container"));
 Game.getRandomPosition();
