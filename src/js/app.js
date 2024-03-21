579f0db1e8f9f87332d344d59ac5c2dbc4312016
AndreyToptaev <Bonafide111@yandex.ru>
2024-03-21 12:20:06 +0300

    fix init

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,5 @@
 import Character from "./init";
-function generateGrid(container) {
-  for (let i = 0; i < 4; i++) {
-    const row = document.createElement("div");
-    row.classList.add("grid-row");
-    for (let j = 0; j < 4; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      row.appendChild(cell);
-    }
-    container.appendChild(row);
-  }
-}
-
-const container = document.querySelector(".grid-container");
-generateGrid(container);
 
 const Game = new Character(document.querySelector(".grid-container"));
+Game.init();
 Game.getRandomPosition();
--- src/js/init.js
+++ src/js/init.js
@@ -4,6 +4,20 @@ export default class Character {
     this.icon = elem.querySelector(".icon");
   }
 
+  init() {
+    const container = document.querySelector(".grid-container");
+
+    for (let i = 0; i < 4; i++) {
+      const row = document.createElement("div");
+      row.classList.add("grid-row");
+      for (let j = 0; j < 4; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        row.appendChild(cell);
+      }
+      container.appendChild(row);
+    }
+  }
   getRandomPosition() {
     let previous = 0;
     let i = 0;
